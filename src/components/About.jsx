5e5e0cb082c213b329afcadfb1678d4f40d9b17d
AndreiParquez <131967438+AndreiParquez@users.noreply.github.com>
2024-05-25 22:50:52 +0800

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,6 +3,7 @@ import p1 from '../assets/p1.png'
 import p2 from '../assets/k.png'
 import p3 from '../assets/h1.png'
 import p4 from '../assets/tagpuan.png'
+import p5 from '../assets/gif.gif'
 import { FiGithub, FiTwitter, FiInstagram, FiFacebook } from "react-icons/fi";
 import { TbBrandGithubFilled } from "react-icons/tb";
 import { FaEye } from "react-icons/fa";
@@ -31,6 +32,43 @@ const About =() => {
             </div>
         <div className='max-w-[1000px] mx-auto p-6 grid md:grid-cols-2 gap-8 place-item-center' id="about">
             
+
+
+
+
+        <div className='relative group'>
+                <div className='relative w-full  p-2 bg-white bg-opacity-10 backdrop-blur-lg rounded-lg'>
+                    <img src={p5} alt="project 1" className='rounded-lg md:max-w-[445px]'/>
+                </div>
+            </div>
+
+           <div className='p-1 mb-20'>
+                <h2 className='text-gray-200 text-2xl font-bold mb-2'>eGrocery <TbWorldWww className='text-zinc-700'/></h2>
+                <p className='text-gray-300 mb-1 text-lg m-0'>
+                  An interactive E learning website about space with stunning ui, made using nodeJs, Javascript, Express, Mysql. 
+                </p>
+                <div className='md:flex flex-wrap gap-4 text-2xl  flex '>
+                <p className='text-green-500 font-bold text-sm'>Tech:</p>
+                    <DiHtml5 className='text-orange-500'/>
+                    <DiCss3 className='text-blue-500'/>
+                    
+                    <DiJavascript1 className='text-yellow-500'/>
+                    <DiNodejsSmall className='text-green-500'/>
+                    <SiMysql className='text-blue-500'/>
+                    <SiExpress className='text-green-400'/>
+                    
+                   
+                    
+                </div>
+                <div className='md:flex flex-wrap gap-4 text-1xl flex justify-center md:justify-start'>
+                <a href="https://github.com/AndreiParquez/Kalawakan-E-Learning" target="_blank" rel="noopener noreferrer">
+                <button className='flex items-center justify-center transform transition-transform hover:scale-105 hover:shadow-lg hover:bg-indigo-950 hover:text-indigo-50 z-10 cursor-pointer font-bold  text-zinc-800 w-24 h-9 mt-6 p-2 bg-white rounded-full'>
+                <TbBrandGithubFilled className='text-2xl font-bold mr-2'/>Github</button></a>
+                <button className='flex items-center justify-center transform transition-transform hover:scale-105 hover:shadow-lg z-10 cursor-pointer font-bold text-white w-24 h-9 mt-6 p-2 bg-zinc-800 rounded-full'>
+                <FaEye className='text-2xl font-bold mr-2'/>view</button>
+                
+                </div>
+            </div>
            
 
 
